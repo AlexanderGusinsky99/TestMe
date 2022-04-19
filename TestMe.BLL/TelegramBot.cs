06c30cd9a15be9bc6ea75d31ca2eeb733abd0a1a
Alexander Gusinsky <alexander.gusinsky99@yandex.ru>
2022-04-19 14:40:44 +0300

    CreateTGBot
    CreatetTGBot, but did'n integrate it

--- TestMe.BLL/TelegramBot.cs
+++ TestMe.BLL/TelegramBot.cs
@@ -1,10 +1,43 @@
 ﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
 using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TestMe.BLL
 {
     public class TelegramBot
     {
-        
+        private TelegramBotClient _client;
+        private Action<string> _onMessage;
+        private List<long> _userIDs;
+        public TelegramBot(string token, Action<string> onMessage)
+        {
+            _client = new TelegramBotClient(token);
+            _onMessage = onMessage;
+            _userIDs = new List<long>();
+        }
+        public void Start()
+        {
+            _client.StartReceiving(HandleResive, HandleError);
+        }
+        private async Task HandleResive(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message != null && update.Message.Text != null) 
+            {
+                if (!_userIDs.Contains(update.Message.Chat.Id))
+                {
+                    _userIDs.Add(update.Message.Chat.Id);
+                }
+            }
+            string userNameAndTextMessage = $"{update.Message.Chat.Id} {update.Message.Chat.FirstName} {update.Message.Chat.LastName}" +
+                $"{update.Message.Text}";
+            _onMessage(userNameAndTextMessage);
+        }
+        private Task HandleError(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        { 
+            return Task.CompletedTask;
+        }
     }
 }
