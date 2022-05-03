213a6b2ce7b17fd10592dcea06f119e80040e519
Korotkov Daniil <dankorotkovwork@gmail.com>
2022-05-03 19:46:12 +0300

    Added working Combobox Alpha

--- TestMe.BLL/Questions/OneAnswerQuestion.cs
+++ TestMe.BLL/Questions/OneAnswerQuestion.cs
@@ -10,7 +10,6 @@ namespace TestMe.BLL
     {
         //Поля или свойства? Пока не ясно, главное, чтобы работало, но потом поправить нужно обязательно
         public string TextOfQuestion { get; set; }
-        public List<string> _listOfAnswers;
         public int _indexOfRightAnswer;
         public OneAnswerQuestion(string textOfQuestion, List<string> listOfAnswers, int indexOfRightAnswer)
         {
@@ -18,5 +17,11 @@ namespace TestMe.BLL
             List<string> _listOfAnswers = listOfAnswers;
             _indexOfRightAnswer = indexOfRightAnswer;
         }
+        public OneAnswerQuestion()
+        {
+
+        }
+
+
     }
 }
